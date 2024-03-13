8aa920960e32fdf5d8f6a2cfe232459eb0e83904
Vaibhavi <141433438+alfredoLoverdotcpp@users.noreply.github.com>
2024-03-13 19:15:34 +0530

    Create NthFibonacci.cpp

---
+++ NthFibonacci.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int fibno(int n){
+  if (n<1){
+    return -1;
+  }
+  else if(n==1){
+    return 1;
+  }
+  else if(n==2){
+    return 2;
+  }
+  else {
+    int a=1;
+    int b=1;
+    int c;
+      for(int i=3; i<n; i++){
+          c=a+b;
+          a=b;
+          b=c;
+        }
+      return b;
+      }
+  }
+
+int main() {
+  int input;
+  cin >> input;
+
+  cout << fibno(n)<<"\n";
+  return 0;
+}
