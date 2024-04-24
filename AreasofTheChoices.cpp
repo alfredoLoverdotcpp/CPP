d33318a20cf4d1c88ff156a4d93a3cd1a60e526f
Vaibhavi <141433438+alfredoLoverdotcpp@users.noreply.github.com>
2024-04-24 20:22:12 +0530

    Update AreasofTheChoices.cpp
    1) took care of the invalid choices from the user by prompting them to add a right number.
    2) added while loop to run infinitely

--- AreasofTheChoices.cpp
+++ AreasofTheChoices.cpp
@@ -1,16 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 double AreasofTheChoices(int ch, vector<double> arr)
 {
     double result=0.0;
     switch(ch)
     {
-        case 1:{
+        case 1:{ //area of circle
           double r = arr[0];
           result = M_PI*r*r;
           break;  
         }
-        case 2: {
+        case 2: { //area of rectangle
             double l = arr[0];
             double b = arr[1];
             result = l*b;
@@ -24,10 +25,17 @@ double AreasofTheChoices(int ch, vector<double> arr)
 }
 
 int main() {
+    while (true){
     int choice;
-    vector <double> array;
+    vector <double> vect;
+    
     cout << "Enter number to find the areas, 1 to find the area of circle and 2 to find the area of rectangle: ";
     cin >> choice;
+    while (choice != 1 && choice != 2) {
+        cout << "Invalid choice! Please enter 1 for circle or 2 for rectangle: ";
+        cin >> choice;
+    }
+    
     if(choice==1 || choice == 2)
     {
         cout << "Enter ";
@@ -36,16 +44,16 @@ int main() {
         }
         else 
         cout << "the values for the length and breadth of the rectangle: ";
-    for (int i=0; i<choice; i++){
+    for (int i=0; i<choice; i++){ 
         double values;
         cin >> values;
-        array.push_back(values);
+        vect.push_back(values);
     }
-    double area = AreasofTheChoices(choice, array);
+    double area = AreasofTheChoices(choice, vect);
     cout << "Area: " << area << endl;
   }
   else
   cout<< "Invalid choice!";
-  
+    }
   return 0;
 }
