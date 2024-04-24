f595bf487ca4f736d624257690e46a06980332f1
Vaibhavi <141433438+alfredoLoverdotcpp@users.noreply.github.com>
2024-04-24 21:15:24 +0530

    Update NthFibonacci.cpp

--- NthFibonacci.cpp
+++ NthFibonacci.cpp
@@ -1,33 +1,33 @@
-#include<iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
 
 int fibno(int n){
-  if (n<1){
-    return -1;
-  }
-  else if(n==1){
-    return 1;
-  }
-  else if(n==2){
-    return 2;
-  }
-  else {
-    int a=1;
-    int b=1;
-    int c;
-      for(int i=3; i<n; i++){
-          c=a+b;
-          a=b;
-          b=c;
-        }
-      return b;
-      }
-  }
-int main() {
-  int input;
-  cin >> input;
+    if(n < 1){
+        return -1;
+    }
+    else if(n == 1 || n == 2){
+        return 1;
+    }
+    else {
+        int a = 1;
+        int b = 1;
+        int c;
 
-  cout << fibno(n)<<"\n";
-  return 0;
+        for(int i = 3; i <= n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
+
+int main() {
+    while (true) {
+        int n;
+        cin >> n;
+
+        cout << "Hi! I am " << fibno(n) << ". I am at position " << n << "\n";
+    }
+    return 0;
 }
