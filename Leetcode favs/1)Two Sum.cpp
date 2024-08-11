058a2a7d078205e170bc6e78f7f84267c1e6f51c
Vaibhavi <141433438+alfredoLoverdotcpp@users.noreply.github.com>
2024-08-11 11:01:42 +0530

    Update 1)Two Sum.cpp

--- Leetcode favs/1)Two Sum.cpp
+++ Leetcode favs/1)Two Sum.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
     int n = nums.size();
-    //Brute Force, TC-O(n^2) and SC- O(1)
+    //**Brute Force, TC-O(n^2) and SC- O(1)**
 
-    // for(int i=0; i<n; ++i){
+//Code:    for(int i=0; i<n; ++i){
     //     for(int j=i+1; j<n; ++j){
     //         int otherNum = target - nums[i];
     //         if(nums[j]==otherNum){
@@ -13,10 +13,11 @@ public:
     //     }
     //   }
 
-    //Better solution, using hashmap
+    //**Better solution, using hashmap**
     //TC - O(N) worst case - O(N^2) SC - O(N)
     // if using map instead of unordered_map, TC - O(NlogN)
 
+//Code:     
      unordered_map<int, int> myMap;
      for(int i=0; i<n; ++i){
          int complement = target - nums[i];
@@ -25,11 +26,14 @@ public:
          }
          myMap[nums[i]]=i;
      }
-        return {};
+    return {};
+        
     //If you don't wanna use a map, then go for 2-pointer approach
-    //But here you're expected to return indices so, this won't be most optimal solution since it'll be stored as (num, index) - eg:{(2,0), (6,1), (5,2), (8, 3)}
+    //But here you're expected to return indices so, this won't be most optimal solution since it'll be stored as (num, index) - eg:{(2,0), (6,1), (5,2), (8, 3)}, TC will be O(N^2)
     //But if you just wanna return YES or NO, then go for this
-    //TC - O(N) + O(NlogN)
+    //TC - O(N) + O(NlogN), SC - no extra space is used hence, O(1) but if the interviewer argues that array changes. 
+    //If the complexity is considered for changed array then SC will be O(N)
+        
     // int left=0, right=n-1;
     // sort(nums.begin(), nums.end())
     // while(left<right){
