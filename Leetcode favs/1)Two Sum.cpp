f5ab4852db6e1f95ee7b4233bb520d3a236601b0
Vaibhavi <141433438+alfredoLoverdotcpp@users.noreply.github.com>
2024-08-11 10:54:37 +0530

    Update 1)Two Sum.cpp

--- Leetcode favs/1)Two Sum.cpp
+++ Leetcode favs/1)Two Sum.cpp
@@ -29,7 +29,7 @@ public:
     //If you don't wanna use a map, then go for 2-pointer approach
     //But here you're expected to return indices so, this won't be most optimal solution since it'll be stored as (num, index) - eg:{(2,0), (6,1), (5,2), (8, 3)}
     //But if you just wanna return YES or NO, then go for this
-
+    //TC - O(N) + O(NlogN)
     // int left=0, right=n-1;
     // sort(nums.begin(), nums.end())
     // while(left<right){
