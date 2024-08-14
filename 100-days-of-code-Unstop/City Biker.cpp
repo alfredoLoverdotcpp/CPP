f95c773b384fce8901a0cbfa15bf2770d2d30442
Vaibhavi <141433438+alfredoLoverdotcpp@users.noreply.github.com>
2024-08-14 19:02:19 +0530

    Update City Biker.cpp
    Gives out highest altitude gain

--- 100-days-of-code-Unstop/City Biker.cpp
+++ 100-days-of-code-Unstop/City Biker.cpp
@@ -1,3 +1,11 @@
+//This code Gives out the highest altitude gain
+
+//Once upon a time, a city biker embarked on an exciting road trip. This journey was laid out across a sequence of n+1 points, each with varying altitudes. 
+//The biker's adventure commenced at point 0, where the altitude was a humble 0 meters above sea level. During his expedition, he encountered different terrains.
+//Each segment was represented by an integer array called gain of length n, where gain[i] denoted the net gain in altitude between points i and i+1.
+//The biker's goal was to find the highest altitude he reached during his journey. 
+//This altitude was determined by the net gain in altitude at each point along the way.
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
